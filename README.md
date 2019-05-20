6d5b8f38508a398229c3db38f9fb723d69bce7be
Kavya <skavya90@gmail.com>
2019-05-20 14:42:36 -0500

    Updated UFO_Visualizations.ipynb

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Java_Script_Homework
 This repository contains data about UFO Sightings with below mentioned files:
    1. data.js - Data about UFO sightings as a list of dictionaries.
-   
-   2. index.html - HTML page to make the data interactive and visual.
-    
-   3. app.js - javscript code that reads the data from dataset into HTML page as a table and enables datasearch based on filters.
 
-   4. image files that are used for HTML background.
+   2. UFO_Visualizations.ipynb - iPython file to generate matplotlib plots.
+   
+   3. index.html - HTML page to make the data interactive and visual.
+
+   4. DataAnalysis.html - Redirect HTML Page from index.html that includes Project details and Analysis with visualization plots.
+    
+   5. app.js - javscript code that reads the data from dataset into HTML page as a table and enables datasearch based on filters.
+
+   6. Data_Analysis.pdf - Writeup on Project Overview and Analysis.
+
+   7. Image files that are used for HTML background.
    
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var $countryInput = document.querySelector("#country");
 var $shapeInput = document.querySelector("#shape");
 var $searchBtn = d3.select("#filter-btn");
 var $resetBtn = d3.select("#reset-btn");
+var $downloadBtn = d3.select("#download-btn");
 // YOUR CODE HERE!
 // Add an event listener to the searchButton and resetButton, call functions when clicked
 //$searchBtn.addEventListener("click", handleSearchButtonClick);
@@ -34,8 +35,51 @@ function renderTable() {
     $tbody.classList.add('tbody-result');
 }
 
-  }
+}
 
+$downloadBtn.on("click", function() {
+  console.log('in download function');
+  var csv = [];
+  var rows = document.querySelectorAll("tr");
+  console.log(rows);  
+    for (var i = 0; i < rows.length; i++) {
+      var row = [], cols = rows[i].querySelectorAll("td, th");
+        
+      for (var j = 0; j < cols.length; j++) 
+        row.push(cols[j].innerText);
+        
+      csv.push(row.join(","));        
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), 'Data.csv');
+})
+
+function downloadCSV(csv, filename) {
+  var csvFile;
+  var downloadLink;
+
+  // CSV file
+  csvFile = new Blob([csv], {type: "text/csv"});
+
+  // Download link
+  downloadLink = document.createElement("a");
+
+  // File name
+  downloadLink.download = filename;
+
+  // Create a link to the file
+  downloadLink.href = window.URL.createObjectURL(csvFile);
+
+  // Hide download link
+  downloadLink.style.display = "none";
+
+  // Add the link to DOM
+  document.body.appendChild(downloadLink);
+
+  // Click download link
+  downloadLink.click();
+}
  
 //function handleSearchButtonClick() {
 $searchBtn.on("click", function() {
